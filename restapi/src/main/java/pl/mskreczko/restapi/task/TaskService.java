20fbdf20339ae4a056a4e3e2b15c334ca49af648
mskreczko <mic.skreczko@gmail.com>
2022-12-23 08:36:10 +0100

    implemented creating and deleting tasks

--- restapi/src/main/java/pl/mskreczko/restapi/task/TaskService.java
+++ restapi/src/main/java/pl/mskreczko/restapi/task/TaskService.java
@@ -2,9 +2,11 @@ package pl.mskreczko.restapi.task;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.mskreczko.restapi.task.dto.TaskContentDto;
 import pl.mskreczko.restapi.task.dto.TaskCreationDto;
 import pl.mskreczko.restapi.task.dto.TaskPreviewDto;
 import pl.mskreczko.restapi.user.User;
+import pl.mskreczko.restapi.user.UserService;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,13 +15,20 @@ import java.util.stream.Collectors;
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
+    private final UserService userService;
 
     public List<TaskPreviewDto> findAllByUsername(String username) {
         return taskRepository.findByUsername(username).stream().map(task -> new TaskPreviewDto(task.getId(), task.getTitle(), task.getStatus())).collect(Collectors.toList());
     }
 
-    public void createNewTask(User user, TaskCreationDto taskCreationDto) {
-        Task task = new Task(taskCreationDto.title(), taskCreationDto.description(), user);
-        taskRepository.save(task);
+    public TaskContentDto createNewTask(String username, TaskCreationDto taskCreationDto) {
+        User user = userService.loadUserByUsername(username);
+        Task task = taskRepository.save(new Task(taskCreationDto.title(), taskCreationDto.description(), user));
+
+        return new TaskContentDto(task.getId(), task.getTitle(), task.getDescription(), task.getCreationDate(), task.getStatus());
+    }
+
+    public void deleteTask(Integer taskId) {
+        taskRepository.deleteById(taskId);
     }
 }
--- restapi/src/test/java/pl/mskreczko/restapi/task/TaskServiceTest.java
+++ restapi/src/test/java/pl/mskreczko/restapi/task/TaskServiceTest.java
@@ -7,7 +7,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.mskreczko.restapi.task.dto.TaskCreationDto;
+import pl.mskreczko.restapi.user.User;
+import pl.mskreczko.restapi.user.UserService;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,8 +21,12 @@ public class TaskServiceTest {
     @Mock
     private TaskRepository taskRepository;
 
+    @Mock
+    private UserService userService;
+
     @InjectMocks
     private TaskService taskService;
+
 
     @Test
     void findAllByUsername_returnsList() {
@@ -42,5 +50,23 @@ public class TaskServiceTest {
         final var actual = taskService.findAllByUsername("test@test.com");
 
         Assertions.assertEquals(0, actual.size());
+    }
+
+    @Test
+    void createTask_returnsTaskContentDto() {
+        final var user = new User("test@test.com", "ZAQ!2wsx");
+        final var dto = new TaskCreationDto("walk the dog", "i need to walk my dog");
+        final var expected = new Task(dto.title(), dto.description(), user);
+
+        Mockito.when(userService.loadUserByUsername("test@test.com")).thenReturn(user);
+        Mockito.when(taskRepository.save(Mockito.any(Task.class))).thenReturn(expected);
+
+        final var actual = taskService.createNewTask("test@test.com", dto);
+
+        Assertions.assertNull(actual.id());
+        Assertions.assertEquals("walk the dog", actual.title());
+        Assertions.assertEquals("i need to walk my dog", actual.description());
+        Assertions.assertEquals(LocalDate.now(), actual.creationDate());
+        Assertions.assertEquals(Status.ACTIVE, actual.status());
     }
 }
