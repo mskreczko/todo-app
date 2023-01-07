4a2b843af2d9f2dc868e5176908522a4b4c80684
mskreczko <mic.skreczko@gmail.com>
2023-01-07 17:38:43 +0100

    fixed typo

--- restapi/src/main/java/pl/mskreczko/restapi/task/TaskRepository.java
+++ restapi/src/main/java/pl/mskreczko/restapi/task/TaskRepository.java
@@ -9,9 +9,9 @@ import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-    @Query(value = "SELECT t from Task t WHERE t.user.email = ?1")
+    @Query(value = "SELECT t FROM Task t WHERE t.user.email = ?1")
     List<Task> findByUsername(String username);
 
-    @Query(value = "SELECT t from Task t WHERE t.user.email = ?2 AND t.id = ?1")
+    @Query(value = "SELECT t FROM Task t WHERE t.user.email = ?2 AND t.id = ?1")
     Task findByIdAndUsername(Integer id, String username);
 }
