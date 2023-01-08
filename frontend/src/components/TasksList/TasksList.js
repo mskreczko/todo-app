679e0e775af08121bd4bb9caeda5eb9a59cdc06c
mskreczko <mic.skreczko@gmail.com>
2023-01-08 18:36:42 +0100

    reimplemented fetching tasks

--- frontend/src/components/TasksList/TasksList.js
+++ frontend/src/components/TasksList/TasksList.js
@@ -3,35 +3,31 @@ import { Link } from 'react-router-dom';
 import './TasksList.css';
 
 export default function TasksList() {
-    const [tasks, setTasks] = useState();
+    const [tasks, setTasks] = useState(null);
 
     useEffect(() => {
         const getTasks = async () => {
-            const data = await(
-                fetch('http://localhost:8080/api/v1/tasks', {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': 'Bearer ' + localStorage.getItem('token'),
-                    },
-                })
-            ).then((response) => response.json());
-
-            setTasks(data);
-        };
+            await fetch('http://localhost:8080/api/v1/tasks', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                },
+            }).then((response) => response.json()).then((tasks) => setTasks(tasks));
+        }
 
         getTasks();
     }, []);
 
     return (
         <ul className='tasks'>
-            {tasks && tasks.map((task, idx) => (
+            {tasks ? tasks.map((task, idx) => (
                 <li className='single-task' key={idx}>
                     <div className='task-preview'>
                         <Link className='single-task-btn' to={`/tasks/${task.id}`}><h2 className='single-task-title'>{task.title}</h2></Link>
                         <Link className='del-icon' to={`/tasks/delete/${task.id}`}><i className='gg-trash'></i></Link>
                     </div>
                 </li>
-            ))}
+            )) : null}
         </ul>
     );
 }
